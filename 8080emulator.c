5a5601c8b87316605d888d0f5fc35f1a2fc76a40
strnikki <nicolefanelli@protonmail.com>
2019-09-12 16:00:38 -0300

    Implements all MVI instructions

--- 8080emulator.c
+++ 8080emulator.c
@@ -53,7 +53,9 @@ int Emulate8080Op(State8080* state)
         case 0x03: UnimplementedInstruction(state); break;
         case 0x04: UnimplementedInstruction(state); break;
         case 0x05: UnimplementedInstruction(state); break;
-        case 0x06: UnimplementedInstruction(state); break;
+        case 0x06:          //MVI B,D8 
+            state->b = opcode[1];
+            state->pc += 1;
         case 0x07: UnimplementedInstruction(state); break;
         case 0x08: UnimplementedInstruction(state); break;
         case 0x09: UnimplementedInstruction(state); break;
@@ -61,7 +63,9 @@ int Emulate8080Op(State8080* state)
         case 0x0b: UnimplementedInstruction(state); break;
         case 0x0c: UnimplementedInstruction(state); break;
         case 0x0d: UnimplementedInstruction(state); break;
-        case 0x0e: UnimplementedInstruction(state); break;
+        case 0x0e:          //MVI C,D8
+            state->c = opcode[1];
+            state->pc += 1;
         case 0x0f: UnimplementedInstruction(state); break;
         case 0x10: UnimplementedInstruction(state); break;
         case 0x11: UnimplementedInstruction(state); break;
@@ -69,7 +73,9 @@ int Emulate8080Op(State8080* state)
         case 0x13: UnimplementedInstruction(state); break;
         case 0x14: UnimplementedInstruction(state); break;
         case 0x15: UnimplementedInstruction(state); break;
-        case 0x16: UnimplementedInstruction(state); break;
+        case 0x16:          //MVI D,D8
+            state->d = opcode[1];
+            state->pc += 1; 
         case 0x17: UnimplementedInstruction(state); break;
         case 0x18: UnimplementedInstruction(state); break;
         case 0x19: UnimplementedInstruction(state); break;
@@ -77,7 +83,9 @@ int Emulate8080Op(State8080* state)
         case 0x1b: UnimplementedInstruction(state); break;
         case 0x1c: UnimplementedInstruction(state); break;
         case 0x1d: UnimplementedInstruction(state); break;
-        case 0x1e: UnimplementedInstruction(state); break;
+        case 0x1e:          //MVI E,D8
+            state->e = opcode[1];
+            state->pc += 1;
         case 0x1f: UnimplementedInstruction(state); break;
         case 0x20: UnimplementedInstruction(state); break;
         case 0x21: UnimplementedInstruction(state); break;
@@ -85,7 +93,9 @@ int Emulate8080Op(State8080* state)
         case 0x23: UnimplementedInstruction(state); break;
         case 0x24: UnimplementedInstruction(state); break;
         case 0x25: UnimplementedInstruction(state); break;
-        case 0x26: UnimplementedInstruction(state); break;
+        case 0x26:          //MVI H,D8
+            state->h = opcode[1];
+            state->pc += 1;
         case 0x27: UnimplementedInstruction(state); break;
         case 0x28: UnimplementedInstruction(state); break;
         case 0x29: UnimplementedInstruction(state); break;
@@ -93,7 +103,9 @@ int Emulate8080Op(State8080* state)
         case 0x2b: UnimplementedInstruction(state); break;
         case 0x2c: UnimplementedInstruction(state); break;
         case 0x2d: UnimplementedInstruction(state); break;
-        case 0x2e: UnimplementedInstruction(state); break;
+        case 0x2e:          //MVI L,D8
+            state->l = opcode[1];
+            state->pc += 1; 
         case 0x2f: UnimplementedInstruction(state); break;
         case 0x30: UnimplementedInstruction(state); break;
         case 0x31: UnimplementedInstruction(state); break;
@@ -101,7 +113,10 @@ int Emulate8080Op(State8080* state)
         case 0x33: UnimplementedInstruction(state); break;
         case 0x34: UnimplementedInstruction(state); break;
         case 0x35: UnimplementedInstruction(state); break;
-        case 0x36: UnimplementedInstruction(state); break;
+        case 0x36:          //MVI M,D8
+            uint16_t offset = (state->h << 8) | (state->l);
+            state->memory[offset] = opcode[1];
+            state->pc += 1; 
         case 0x37: UnimplementedInstruction(state); break;
         case 0x38: UnimplementedInstruction(state); break;
         case 0x39: UnimplementedInstruction(state); break;
@@ -109,7 +124,9 @@ int Emulate8080Op(State8080* state)
         case 0x3b: UnimplementedInstruction(state); break;
         case 0x3c: UnimplementedInstruction(state); break;
         case 0x3d: UnimplementedInstruction(state); break;
-        case 0x3e: UnimplementedInstruction(state); break;
+        case 0x3e:          //MVI A,D8
+            state->a = opcode[1];
+            state->pc += 1;
         case 0x3f: UnimplementedInstruction(state); break;
         case 0x40: UnimplementedInstruction(state); break;
         case 0x41: state->b = state->c; break;  //MOV B,C 
